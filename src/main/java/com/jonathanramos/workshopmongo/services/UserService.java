66446d3f4a9988857c1c632946f08c99f3066a94
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-12-30 19:53:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/jonathanramos/workshopmongo/services/UserService.java
+++ src/main/java/com/jonathanramos/workshopmongo/services/UserService.java
@@ -7,21 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonathanramos.workshopmongo.domain.User;
+import com.jonathanramos.workshopmongo.dto.UserDTO;
 import com.jonathanramos.workshopmongo.repository.UserRepository;
 import com.jonathanramos.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
