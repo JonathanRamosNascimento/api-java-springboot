34dcf6b554b939d80bf2801cd78cfb731a382574
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-01-02 22:14:19 -0300

    Consulta simples com query methods

--- src/main/java/com/jonathanramos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jonathanramos/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.jonathanramos.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.jonathanramos.workshopmongo.enetity.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
